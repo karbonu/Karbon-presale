2e6fdc0d97d3ad49592de1b172701c4911eee14d
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-24 22:29:36 +0100

    "Added contribute section with payment method options and conditional rendering of payment method components based on selected method"

--- src/Pages/_root/RootPages/TokenSale.tsx
+++ src/Pages/_root/RootPages/TokenSale.tsx
@@ -453,6 +453,77 @@ const TokenSale = () => {
                         </div>
                       </div>
                     </div>
+
+                    <p className="text-white  font-bold text-[20px]">Contribute</p>
+                    <div className="">
+                        <div
+                          className="fade-transition"
+                          style={{ opacity: selectedMethod === 0 ? 1 : 0 }}
+                        >
+                          {/* Content for selectedMethod === 0 */}
+                          {selectedMethod === 0 && (
+                            <div className="flex flex-col space-y-2 items-center justify-center">
+                              <div onClick={() => setSelectedMethod(1)} className="w-full flex items-center px-3 cursor-pointer hover:border-[#08E04A] border-[1px] border-transparent transition ease-in-out rounded-[4px] bg-[#1C1C1C] h-[40px]">
+                                <div className="flex flex-row w-full items-center justify-between ">
+                                  <CreditCardlogo />
+                                  <p className="text-white text-[14px]">Buy with Credit Card</p>
+                                  <ForwardIcon />
+                                </div>
+                              </div>
+
+                              <div onClick={() => {setIsDialogOpen(true); setSelectedMethod(2)}} className="w-full flex items-center px-3 cursor-pointer hover:border-[#08E04A] border-[1px] border-transparent transition ease-in-out rounded-[4px] bg-[#1C1C1C] h-[40px]">
+                                <div className={`flex flex-row w-full items-center ${!isConnected ? 'justify-between' : ''}`}>
+                                  <USDTLogoBig />
+                                  <p 
+                                  className={`text-white text-[14px] ${isConnected ? "text-center w-full" : "pl-10"}`}
+                                  >Buy with USDT</p>
+                                  {!isConnected && (
+                                    <p className="text-[#08E04A] text-[10px]">Connect Wallet</p>
+                                  )}
+                                </div>
+                              </div>
+
+                              <div onClick={() => setSelectedMethod(3)} className="w-full flex items-center px-3 cursor-pointer hover:border-[#08E04A] border-[1px] border-transparent transition ease-in-out rounded-[4px] bg-[#1C1C1C] h-[40px]">
+                                <div className="flex flex-row w-full items-center justify-between ">
+                                  <PaypalLogo />
+                                  <p className="text-white text-[14px]">Buy with PayPal</p>
+                                  <ForwardIcon />
+                                </div>
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                        <div
+                          className="fade-transition "
+                          style={{ opacity: selectedMethod === 1 ? 1 : 0 }}
+                        >
+                          {/* Content for selectedMethod === 1 */}
+                          {selectedMethod === 1 && <BuyWithCreditCard setSelectedMethod={setSelectedMethod} />}
+                        </div>
+
+                        <div
+                          className="fade-transition "
+                          style={{ opacity: selectedMethod === 2 ? 1 : 0 }}
+                        >
+                          {/* Content for selectedMethod === 2 */}
+                          {selectedMethod === 2 && (
+                            <BuyWithUSDT
+                              setSelectedMethod={setSelectedMethod}
+                              isDialogOpen={isDialogOpen}
+                              setIsDialogOpen={setIsDialogOpen}
+                            />
+                          )}
+                        </div>
+
+                        <div
+                          className="fade-transition "
+                          style={{ opacity: selectedMethod === 3 ? 1 : 0 }}
+                        >
+                          {/* Content for selectedMethod === 3 */}
+                          {selectedMethod === 3 && <BuyWithPaypal setSelectedMethod={setSelectedMethod} />}
+                        </div>
+                      
+                    </div>
                 </div>
               </div>
 
