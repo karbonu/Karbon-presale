9fe21abde8c5907be686fde43feae8cb8c1c9de3
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-24 20:26:15 +0100

    "Updated SignIn.tsx with responsive design changes for mobile devices"

--- src/Pages/_auth/AuthPages/SignIn.tsx
+++ src/Pages/_auth/AuthPages/SignIn.tsx
@@ -12,13 +12,13 @@ const SignIn = () => {
     const [step, setStep] = useState(1);
   return (
     <div className="p-[60px]">
-        <div>
+        <div className=" max-sm:flex max-sm: items-center max-sm:justify-center">
             <KarbonLogo/>
         </div>
 
         <div className="flex items-center justify-center w-full flex-col">
             {chanceInfo && (
-                <div className="flex absolute top-20 flex-row space-x-3 items-center justify-between pl-5 rounded-[8px] border-[1px] border-[#282828] w-[421px] h-[52px] bg-black">
+                <div className="flex absolute top-20 flex-row space-x-3 items-center justify-between pl-5 rounded-[8px] border-[1px] border-[#282828] max-sm:w-[80%] max-sm:top-24 w-[421px] h-[52px] bg-black">
                     <div onClick={() => setChannceInfo(false)}>
                         <CloseIcon/>
                     </div>
@@ -36,29 +36,29 @@ const SignIn = () => {
 
             {step === 1 &&(
                  <div className="flex flex-col items-center justify-center pt-[7rem] " >
-                    <div className="w-[450px] h-[456px] bg-[#101010] border-[#2D2D2D] border-[1px] rounded-[8px]">
+                    <div className="w-[450px] max-sm:w-[300px] h-[456px] bg-[#101010] border-[#2D2D2D] border-[1px] rounded-[8px]">
     
                         <div className="py-5 px-8 flex flex-col justify-between h-full">
-                            <p className="text-white text-[20px] font-semibold">Sign In</p>
+                            <p className="text-white text-[20px] max-sm:text-[90%] font-semibold">Sign In</p>
     
-                            <div className="flex flex-col space-y-2">
-                                <div className=" flex flex-row w-[389px] h-[56px] bg-[#1C1C1C]">
-                                    <div className="px-5 absolute py-3 flex items-center">
+                            <div className="flex flex-col space-y-2 ">
+                                <div className=" flex flex-row w-[389px] max-sm:w-[100%] max-sm:h-[48px]  h-[56px] bg-[#1C1C1C]">
+                                    <div className="px-5 absolute max-sm:py-[0.7rem] py-3 flex items-center">
                                         <GoogleLogo/>
                                     </div>
                                     <div className=" flex-1 flex items-center justify-center">
-                                        <p className="text-white text-[14px]">Sign in with Google</p>
+                                        <p className="text-white text-[14px] max-sm:text-[10px]">Sign in with Google</p>
     
                                     </div>
     
                                 </div>
     
-                                <div className=" flex flex-row w-[389px] h-[56px] bg-[#1C1C1C]">
-                                    <div className="px-7 absolute py-5 flex items-center">
+                                <div className=" flex flex-row w-[389px] max-sm:w-[100%] max-sm:h-[48px]  h-[56px] bg-[#1C1C1C]">
+                                    <div className="px-7 absolute max-sm:py-[0.7rem] py-5 flex items-center">
                                         <AppleLogo/>
                                     </div>
                                     <div className=" flex-1 flex items-center justify-center">
-                                        <p className="text-white text-[14px]">Sign in with Apple ID</p>
+                                        <p className="text-white text-[14px] max-sm:text-[10px]">Sign in with Apple ID</p>
     
                                     </div>
     
@@ -67,9 +67,9 @@ const SignIn = () => {
                             </div>
     
                             <div className="flex flex-row space-x-4 items-center justify-center">
-                                <Separator orientation="horizontal" className="max-w-[27%]"/>
-                                <p className="text-white text-[14px]">or sign in with email</p>
-                                <Separator orientation="horizontal" className="max-w-[27%]"/>
+                                <Separator orientation="horizontal" className="max-w-[27%] max-sm:max-w-[20%]"/>
+                                <p className="text-white text-[14px] max-sm:text-[10px]">or sign in with email</p>
+                                <Separator orientation="horizontal" className="max-w-[27%] max-sm:max-w-[20%]"/>
                             </div>
     
                             <div className="flex flex-col space-y-5">
@@ -85,8 +85,8 @@ const SignIn = () => {
                             </div>
     
                             <div className="flex flex-row space-x-2 items-center justify-center">
-                                <p className="text-white text-[14px]">Don't have an account?</p>
-                                <a href="/sign-up" className="text-[14px] text-[#08E04A] font-semibold">Sign Up</a>
+                                <p className="text-white text-[14px] max-sm:text-[10px]">Don't have an account?</p>
+                                <a href="/sign-up" className="text-[14px] max-sm:text-[10px] text-[#08E04A] font-semibold">Sign Up</a>
                             </div>
                         </div>
     
@@ -98,12 +98,12 @@ const SignIn = () => {
 
             {step ===2 && (
                  <div className="flex flex-col items-center justify-center pt-[7rem] " >
-                 <div className="w-[450px] h-[346px] bg-[#101010] border-[#2D2D2D] border-[1px] rounded-[8px]">
+                 <div className="w-[450px] max-sm:w-[300px] h-[346px] bg-[#101010] border-[#2D2D2D] border-[1px] rounded-[8px]">
  
                      <div className="py-5 px-8 flex flex-col justify-between h-full">
                         <div className="flex flex-row space-x-2 items-center">
                             <PasswordLogo/>
-                            <p className="text-white text-[20px] font-semibold">Enter Password</p>
+                            <p className="text-white text-[20px] max-sm:text-[16px] font-semibold">Enter Password</p>
                         </div>
                         <div className="flex flex-row space-x-1 items-center">
                             <p className="text-white opacity-80 text-[14px]">Welcome</p>
@@ -141,7 +141,7 @@ const SignIn = () => {
             )}
 
         </div>
-        <div className=" absolute bottom-10 left-[43.5%]">
+        <div className=" md:absolute bottom-10 max-sm:flex max-sm:items-center max-sm:justify-center max-sm:py-10 left-[43.5%]">
             <p className="text-white text-[10px] opacity-50">Copyright © 2024 Karbon. All rights reserved.</p>
         </div>
         
