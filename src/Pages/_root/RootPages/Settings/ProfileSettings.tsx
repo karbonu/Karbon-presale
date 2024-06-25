ed988528062f489f2faaca51cc4d02d27ea96033
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-25 22:38:51 +0100

    "Updated styles and layouts for ProfileSettings, SettingsLayout, and WalletSettings components to improve responsiveness and mobile compatibility."

--- src/Pages/_root/RootPages/Settings/ProfileSettings.tsx
+++ src/Pages/_root/RootPages/Settings/ProfileSettings.tsx
@@ -8,26 +8,26 @@ const ProfileSettings = () => {
         <MetaTags
         title="Karbon Sale | Profile Settings"
         />
-    <div className="flex flex-row w-full space-x-2 justify-between">
-      <div className="border-[1px] border-[#282828] h-[95px] min-w-[364px] bg-black rounded-[8px]">
+    <div className="flex flex-row max-lg:flex-col max-lg:space-y-2 w-full lg:space-x-2 justify-between">
+      <div className="border-[1px] border-[#282828] h-[95px] md:min-w-[364px] max-lg:w-full bg-black rounded-[8px]">
         <div className="p-3 flex-col space-y-3 h-full justify-between">
           <div className="flex flex-row rounded-[2px]  justify-center py-1 space-x-1 items-center border-[1px] border-[#282828] max-w-[54px]">
             <WalletIcon/>
             <p className="text-white text-[10px] opacity-70">Email</p>
           </div>
           <div className="flex flex-row items-center space-x-5">
-            <p className="text-white font-light text-[14px] ">you@gmail.com</p>
+            <p className="text-white font-light text-[14px] max-lg:text-[12px] ">you@gmail.com</p>
           </div>
         </div>
       </div>
-      <div className="border-[1px] border-[#282828] h-[95px] min-w-[364px] bg-black rounded-[8px]">
+      <div className="border-[1px] border-[#282828] h-[95px]  md:min-w-[364px] max-lg:w-full bg-black rounded-[8px]">
         <div className="p-3 flex-col space-y-3 h-full justify-between">
           <div className="flex flex-row rounded-[2px]  justify-center py-1 space-x-1 items-center border-[1px] border-[#282828] max-w-[97px]">
             <WalletIcon/>
             <p className="text-white text-[10px] opacity-70">Referral Link</p>
           </div>
           <div className="flex flex-row items-center space-x-5">
-            <p className="text-white font-light text-[14px] ">https://karbon.com/78236-tube...</p>
+            <p className="text-white font-light text-[14px] max-lg:text-[12px] ">https://karbon.com/78236-tube...</p>
             <div className="flex flex-row space-x-2 cursor-pointer">
               <CopyIcon/>
               <p className="text-[#08E04A] text-[10px]">Copy</p>
@@ -38,19 +38,19 @@ const ProfileSettings = () => {
 
     </div>
 
-    <div className="flex flex-1">
-      <div className="bg-black border-[1px] border-[#282828] rounded-[8px]">
-        <div className="flex flex-col p-5 space-y-4">
+    <div className="flex flex-1 max-lg:w-full">
+      <div className="bg-black border-[1px] border-[#282828] max-lg:w-full rounded-[8px]">
+        <div className="flex flex-col p-5 space-y-4 max-lg:w-full">
           <p className="text-white font-medium text-[20px]">Change Password</p>
-          <div className="flex flex-row w-full items-center justify-between space-x-2">
-            <div className="flex flex-col space-y-2">
+          <div className="flex flex-row max-lg:flex-col max-lg:space-y-2 w-full items-center justify-between md:space-x-2">
+            <div className="flex flex-col max-lg:w-full space-y-2">
               <p className="text-white text-[12px]">New Password</p>
-              <input type="password" className=" outline-none focus:outline-[1px] focus:outline-[#282828] bg-[#181818] w-[347px] h-[40px] text-white"/>
+              <input type="password" className=" outline-none focus:outline-[1px] focus:outline-[#282828] bg-[#181818] w-[347px] max-lg:w-[100%] h-[40px] text-white"/>
             </div>
 
-            <div className="flex flex-col space-y-2">
+            <div className="flex flex-col max-lg:w-full space-y-2">
               <p className="text-white text-[12px]">Confirm New Password</p>
-              <input type="password" className=" outline-none focus:outline-[1px] focus:outline-[#282828] bg-[#181818] w-[347px] h-[40px] text-white"/>
+              <input type="password" className=" outline-none focus:outline-[1px] focus:outline-[#282828] bg-[#181818] w-[347px] max-lg:w-[100%] h-[40px] text-white"/>
             </div>
 
           </div>
@@ -66,8 +66,8 @@ const ProfileSettings = () => {
 
     </div>
 
-    <div className="flex flex-row pt-10 justify-between items-center">
-        <div className="flex flex-row space-x-40">
+    <div className="flex flex-row max-lg:flex-col pt-10 justify-between items-center max-lg:space-y-2">
+        <div className="flex flex-row lg:space-x-40 max-lg:w-full max-lg:justify-between max-sm:px-5">
         <p className="text-[8px] text-white opacity-30">Copyright © 2024 Karbon. All rights reserved.</p>
         <p className="text-[8px] text-white opacity-30">Gaziantep, Türkiye</p>
         </div>        
--- src/Pages/_root/RootPages/Settings/WalletSettings.tsx
+++ src/Pages/_root/RootPages/Settings/WalletSettings.tsx
@@ -12,19 +12,19 @@ const {address} = useAccount();
   const { disconnect } = useDisconnect()
 
   return (
-    <div className="flex flex-col pt-10 space-y-4 justify-between">
+    <div className="flex flex-col max-lg:w-full max-sm:max-w-[100%] pt-10 space-y-4 justify-between">
         <MetaTags
         title="Karbon Sale | Waallet Settings"
         />
          
-        <div className="border-[1px] border-[#282828] max-w-[740px] h-[95px] bg-black rounded-[8px]">
+        <div className="border-[1px] border-[#282828] max-w-[740px] max-lg:w-full max-sm:h-full h-[95px] bg-black rounded-[8px]">
             <div className="p-3 flex-col space-y-3 h-full justify-between">
-                <div className="flex flex-row rounded-[2px]  justify-center py-1 space-x-1 items-center border-[1px] border-[#282828] max-w-[110px]">
+                <div className="flex flex-row rounded-[2px] justify-center py-1 space-x-1 items-center border-[1px] border-[#282828] max-w-[110px]">
                 <WalletIcon/>
                 <p className="text-white text-[10px] opacity-70">WALLET ADDRESS</p>
                 </div>
-                <div className="flex flex-row items-center space-x-5">
-                <p className="text-white text-[16px] font-semibold">
+                <div className="flex flex-row max-sm:flex-col max-sm:space-y-2  md:items-center md:space-x-5">
+                <p className="text-white text-[16px] max-sm:font-normal max-sm:text-[10px] font-semibold">
                     {!address ? (
                         "Connect Your Wallet"
                     ): (
@@ -43,18 +43,18 @@ const {address} = useAccount();
         </div>
     </div>
 
-    <div className="border-[1px] border-[#282828] max-w-[740px] h-[95px] bg-black rounded-[8px]">
+    <div className="border-[1px] border-[#282828] max-w-[740px] max-lg:w-full max-sm:h-full h-[95px] bg-black rounded-[8px]">
     <div className="p-3 flex-col space-y-3 h-full justify-between">
         <div className="flex flex-row rounded-[2px]  justify-center py-1 space-x-1 items-center border-[1px] border-[#282828] max-w-[73px]">
         <KeyIcon/>
         <p className="text-white text-[10px] opacity-70">Network</p>
         </div>
-        <p className="text-white text-[16px] font-semibold">Ethereum</p>
+        <p className="text-white text-[16px] max-sm:font-normal max-sm:text-[10px] font-semibold">Ethereum</p>
     </div>
     </div>
 
-    <div className="flex flex-row pt-10 justify-between items-center">
-        <div className="flex flex-row space-x-40">
+    <div className="flex flex-row max-lg:flex-col pt-10 justify-between items-center max-lg:space-y-2">
+        <div className="flex flex-row lg:space-x-40 max-lg:w-full max-lg:justify-between max-sm:px-5">
         <p className="text-[8px] text-white opacity-30">Copyright © 2024 Karbon. All rights reserved.</p>
         <p className="text-[8px] text-white opacity-30">Gaziantep, Türkiye</p>
         </div>
