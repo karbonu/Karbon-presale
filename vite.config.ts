6f20fac7f8908e254968b29f0cd3fd49bc81368e
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-06 09:09:30 +0100

    env update

--- vite.config.ts
+++ vite.config.ts
@@ -18,24 +18,21 @@ import { defineConfig, loadEnv } from "vite"
 // })
 
 
+
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
-  
-    define: {
-      plugins: [react()],
-        resolve: {
-          alias: {
-            "@": path.resolve(__dirname, "./src"),
-          },
-        },
-      define: {
-        VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
-        VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
-        VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  }
-})
+    define: {
+      VITE_WALLET_CONNECT_PROJECT_ID: JSON.stringify(env.VITE_WALLET_CONNECT_PROJECT_ID),
+      VITE_BACKEND_API_URL: JSON.stringify(env.VITE_BACKEND_API_URL),
+      VITE_GOOGLE_CLIENT_ID: JSON.stringify(env.VITE_GOOGLE_CLIENT_ID),
+    },
+  };
+});
