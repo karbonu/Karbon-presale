5b9b3fbf1b502dc03418f5a27a2d461814e7f74f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-06 08:51:27 +0100

    env update

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
+    VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
+    VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+  },
 })
