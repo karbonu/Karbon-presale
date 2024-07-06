a664f9776920af33e3a18935738093507ab58b6e
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-06 09:05:47 +0100

    env update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,41 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// export default defineConfig(({ command, mode }) =>({
+//   const env = loadEnv(mode, process.cwd(), ''),
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   define: {
+//     VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
+//     VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
+//     VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+//   },
+// })
+
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+  
+    define: {
+      plugins: [react()],
+        resolve: {
+          alias: {
+            "@": path.resolve(__dirname, "./src"),
+          },
+        },
+      define: {
+        VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
+        VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
+        VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+      },
     },
-  },
-  define: {
-    VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
-    VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
-    VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
-  },
+  }
 })
