afa73611e5dfd2de5241dedcf0284355233f41b6
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-06 07:48:27 +0100

    "Removed and commented out imports and code blocks in AdminConnectWallet.tsx and updated URL in getTotalContribution function in TokenSaleHooks.tsx"

--- src/Admin/AdminPages/AdminConnectWallet.tsx
+++ src/Admin/AdminPages/AdminConnectWallet.tsx
@@ -3,32 +3,32 @@
 import ConnectIconGreen from "@/components/Icons/ConnectIconGreen";
 import KarbonLogo from "@/components/Icons/KarbonLogo";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useAdminAuth } from "../Hooks/AdminAuthContext";
-import { useNavigate } from "react-router-dom";
-import { useAccount } from "wagmi";
-import { useEffect } from "react";
+// import { useAdminAuth } from "../Hooks/AdminAuthContext";
+// import { useNavigate } from "react-router-dom";
+// import { useAccount } from "wagmi";
+// import { useEffect } from "react";
 
 const AdminConnectWallet = () => {
-  const { connectWallet, disconnectWaller } = useAdminAuth();
+  // const { connectWallet, disconnectWaller } = useAdminAuth();
   const { open } = useWeb3Modal();
-  const navigate = useNavigate();
-  const {isConnected} = useAccount();
+  // const navigate = useNavigate();
+  // const {isConnected} = useAccount();
 
   const handleConnectWallet = () => {
     open();
   };
 
 
-  useEffect(() => {
-    if(!isConnected){
-      disconnectWaller();
-      navigate('/adminSignin');
-    }else{
-      connectWallet();
-      navigate('/admin');
-    }
+  // useEffect(() => {
+  //   if(!isConnected){
+  //     disconnectWaller();
+  //     navigate('/adminSignin');
+  //   }else{
+  //     connectWallet();
+  //     navigate('/admin');
+  //   }
     
-  }, [isConnected]);
+  // }, [isConnected]);
 
   return (
     <div className="flex flex-col min-h-[100vh] items-center justify-center space-y-5">
