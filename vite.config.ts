f9e2f097116e1361d756b8fc0ee88d40a98e4a25
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-06 08:58:14 +0100

    env update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-
-export default defineConfig(() => {
-  return {
-    define: {
-      VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
-      VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
-      VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
-});
+  },
+  define: {
+    VITE_WALLET_CONNECT_PROJECT_ID: process.env.VITE_WALLET_CONNECT_PROJECT_ID,
+    VITE_BACKEND_API_URL: process.env.VITE_BACKEND_API_URL,
+    VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID
+  },
+})
