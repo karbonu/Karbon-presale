5c377216ba950ce155172c091701cc115f3d7618
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-07-17 15:04:30 +0100

    Remove metaMask import and comment out metaMask connector in config.

--- src/components/shared/Constants/config.ts
+++ src/components/shared/Constants/config.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from 'wagmi'
 import { bscTestnet } from 'wagmi/chains'
-import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+import { injected, safe, walletConnect } from 'wagmi/connectors'
 
 export const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
@@ -9,7 +9,7 @@ export const config = createConfig({
   connectors: [
     injected(),
     walletConnect({ projectId }),
-    metaMask(),
+    // metaMask(),
     safe(),
   ],
   transports: {
