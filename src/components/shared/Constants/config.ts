50efade2dcfc6f6f6ec809512c7aedd79cf4fe81
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-08-13 21:19:01 +0100

    "Added metaMask import from 'wagmi/connectors' in config.ts"

--- src/components/shared/Constants/config.ts
+++ src/components/shared/Constants/config.ts
@@ -1,6 +1,7 @@
 import { http, createConfig } from 'wagmi'
 import { bscTestnet } from 'wagmi/chains'
 import { injected, safe, walletConnect } from 'wagmi/connectors'
+import { metaMask } from 'wagmi/connectors';
 
 export const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
