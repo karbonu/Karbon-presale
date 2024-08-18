6c593aaa22733356afcad943fab6363dbdaacb2b
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-08-18 15:36:05 +0100

    "Update styles in PayoutModalSuccess component"

--- src/components/shared/PayoutModalSuccess.tsx
+++ src/components/shared/PayoutModalSuccess.tsx
@@ -11,9 +11,9 @@ const PayoutModalSuccess = (props: any) => {
     return (
         <div>
             <Dialog open={props.isDialogOpen} onOpenChange={props.setIsDialogOpen}>
-                <DialogContent className='bg-[#101010] border-[1px] border-[#282828] p-10 flex flex-col w-[380px] max-sm:w-[300px] items-center justify-center rounded-[16px] outline-none'>
+                <DialogContent className='bg-[#101010] border-[1px] border-[#282828] p-10 flex flex-col w-[380px] max-sm:w-[95%] items-center justify-center rounded-[16px] outline-none'>
                     <div className='w-full flex flex-row items-center justify-between'>
-                        <p className="text-white font-semibold text-[16px]">{t('payout')}</p>
+                        <p className="text-white font-semibold text-[16px] max-sm:text-[14px]">{t('payout')}</p>
                         <div onClick={() => props.setIsDialogOpen(false)} className='cursor-pointer '>
                             <DialogClose />
                         </div>
@@ -23,14 +23,14 @@ const PayoutModalSuccess = (props: any) => {
                         <SucccessIconSmall />
                         <div className="flex flex-col items-center justify-center space-y-2">
                             <div className="flex flex-row space-x-2">
-                                <p className="text-[#08E04A] font-medium text-[32px]">{props.tokenAmount}</p>
-                                <p className="text-[#08E04A] text-[32px] opacity-50">USDT</p>
+                                <p className="text-[#08E04A] font-medium text-[32px] max-sm:text-[24px]">{props.tokenAmount}</p>
+                                <p className="text-[#08E04A] text-[32px] max-sm:text-[24px] opacity-50">USDT</p>
                             </div>
                             <p className="text-[14px] text-white opacity-70">{t('payoutProcessing')}</p>
                         </div>
-                        <p className="text-center text-white w-[251px] text-[14px] leading-6 tracking-normal">{t('payoutRequestReceived')}</p>
+                        <p className="text-center text-white w-[251px] text-[14px] max-sm:text-[12px] leading-6 tracking-normal">{t('payoutRequestReceived')}</p>
 
-                        <div onClick={() => props.setIsDialogOpen(false)} className="bg-transparent py-2 px-10 cursor-pointer hover:border-[#08E04A] transition ease-in-out text-white text-[14px] hover:text-[#08E04A] rounded-full border-[1px] border-white">
+                        <div onClick={() => props.setIsDialogOpen(false)} className="bg-transparent py-2 px-10 cursor-pointer hover:border-[#08E04A] transition ease-in-out text-white text-[14px] max-sm:text-[12px] hover:text-[#08E04A] rounded-full border-[1px] border-white">
                             {t('close')}
                         </div>
                     </div>
