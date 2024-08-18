a6eb3da54eb01abea4712aea4c2f0b5b3fa3e4e4
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-08-18 15:26:52 +0100

    "Update styles in PayoutModalFailure component: adjust width, font sizes, and padding for different screen sizes"

--- src/components/shared/PayoutModalFaliure.tsx
+++ src/components/shared/PayoutModalFaliure.tsx
@@ -47,23 +47,23 @@ const PayoutModalFailure = (props: any) => {
   return (
     <div>
       <Dialog open={props.isDialogOpen} onOpenChange={props.setIsDialogOpen}>
-        <DialogContent className='bg-[#101010] border-[1px] border-[#282828] py-10 flex flex-col w-[412px] max-sm:w-[300px] items-center justify-center rounded-[16px] outline-none'>
+        <DialogContent className='bg-[#101010] border-[1px] border-[#282828] py-10 flex flex-col w-[412px] max-sm:w-[95%] items-center justify-center rounded-[16px] outline-none'>
           <div className='w-full flex px-8 flex-row items-center justify-between'>
-            <p className="text-white font-semibold text-[16px]">{t('notThereYet')}</p>
+            <p className="text-white font-semibold max-sm:text-[14px] text-[16px]">{t('notThereYet')}</p>
             <div onClick={() => props.setIsDialogOpen(false)} className='cursor-pointer '>
               <DialogClose />
             </div>
           </div>
           <div className="w-full py-5 flex flex-col space-y-6">
-            <p className="text-white px-8 text-[14px]">{t('purchaseMin')} <span className="font-bold">5000 USDT</span> {t('eligiblePayout')}</p>
+            <p className="text-white px-8 text-[14px] max-sm:text-[12px]">{t('purchaseMin')} <span className="font-bold">5000 USDT</span> {t('eligiblePayout')}</p>
             <div className="w-full">
               <Separator className="w-full bg-black h-[2px]" />
             </div>
-            <p className="text-white px-8 text-[14px]">{t('copyReferral')}</p>
+            <p className="text-white px-8 text-[14px] max-sm:text-[12px]">{t('copyReferral')}</p>
             <div className="flex flex-col space-y-2 px-8 max-sm:px-5">
               <div className="flex flex-row w-max items-center py-2 px-4 max-sm:px-2 bg-black space-x-10 max-sm:space-x-2">
                 <div>
-                  <p className="text-white text-[12px]">{ReferralLink.slice(0, 30)}...</p>
+                  <p className="text-white text-[12px] max-sm:text-[10px]">{ReferralLink.slice(0, 30)}...</p>
                 </div>
                 <div onClick={handleCopy} className="flex flex-row items-center space-x-1 cursor-pointer">
                   {copied ? <CheckMark /> : <CopyIcon />}
@@ -73,7 +73,7 @@ const PayoutModalFailure = (props: any) => {
                 </div>
               </div>
               <div className="flex flex-row items-center space-x-5">
-                <p className="text-[12px] opacity-50 text-white">{t('shareOn')}</p>
+                <p className="text-[12px] max-sm:text-[10px] opacity-50 text-white">{t('shareOn')}</p>
                 <a onClick={() => props.setIsDialogOpen(false)} href={discordShareUrl} target="blank" className="opacity-50 hover:opacity-100 cursor-pointer hover:scale-110 transition ease-in-out">
                   <DiscordLogo />
                 </a>
